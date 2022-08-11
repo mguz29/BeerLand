174e6e76d7b927f9b9e2a48c29d524d288d5ff1f
Marlon Guzman <marlonguzman2997@gmail.com>
2022-08-11 00:13:52 -0500

    Update SaleDetail.jsx

--- client/src/components/Purchases/UserPurchases/SaleDetail/SaleDetail.jsx
+++ client/src/components/Purchases/UserPurchases/SaleDetail/SaleDetail.jsx
@@ -10,13 +10,12 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import styles from "./styles.module.css";
 
-export default function SaleDetail({ purchase }) {
+export default function SaleDetail({ purchase, mostrar, setMostrar }) {
   const TAX_RATE = 0.05;
   const invoiceSubtotal = purchase.totalPrice;
   const invoiceTaxes = TAX_RATE * invoiceSubtotal;
   const invoiceTotal = invoiceSubtotal - invoiceTaxes;
-  const [mostrar, setMostrar] = useState(false);
-console.log(purchase);
+  
   function ccyFormat(num) {
     return `${num.toFixed(2)}`;
   }
@@ -24,9 +23,9 @@ console.log(purchase);
   return (
    
         <div className={styles.table}>         
-         
+      { mostrar ?
             <div>
-              
+               <p onClick={setMostrar(false)}>❌</p>
               <TableContainer component={Paper}>
                 <Table
                   sx={{ minWidth: 600, maxWidth: 1000 }}
@@ -83,15 +82,9 @@ console.log(purchase);
                   </TableBody>
                 </Table>
               </TableContainer>
-              <Button
-                variant="contained"
-                color="warning"
-                onClick={() => setMostrar(false)}
-              >
-                Close
-              </Button>
+              
             </div>
-          
+          : ''}
         </div>
       
   );
